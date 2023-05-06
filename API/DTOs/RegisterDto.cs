0d4c7482aed83cad72a056e149eff991f4bd6f1e
Andreas Nygård <andreas-nygard@hotmail.com>
2023-05-06 14:59:56 +0300

    committing missing files

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -7,6 +7,7 @@ namespace API.DTOs
         [Required]
         public string Name { get; set; }
         [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
